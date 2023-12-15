164060ca3fa61a0589dbf7c560987eab4d6fe260
Raman Pandey <102546029+R-A-M-A-N-P-A-N-D-E-Y@users.noreply.github.com>
2023-12-15 21:40:11 +0530

    Time: 54 ms (92.04%), Space: 16.3 MB (74.58%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.py
@@ -0,0 +1,13 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        cities = set()
+
+        # Collect outgoing cities
+        for path in paths:
+            cities.add(path[0])
+
+        # Find destination city with no outgoing path
+        for path in paths:
+            dest = path[1]
+            if dest not in cities:
+                return dest
