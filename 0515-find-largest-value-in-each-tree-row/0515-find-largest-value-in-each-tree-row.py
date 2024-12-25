448c01294c6948057cb6f1135958c1eb5bf98ade
Raman Pandey <102546029+R-A-M-A-N-P-A-N-D-E-Y@users.noreply.github.com>
2024-12-25 23:14:23 +0530

    Time: 7 ms (6.59%), Space: 19.7 MB (8.63%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.py
@@ -6,24 +6,15 @@
 #         self.right = right
 class Solution:
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
+        arr=[]
+        def preOrder(Node, level):
+            if not Node: return
+            if len(arr)<=level: arr.append(-2**31)
+            arr[level]=max(arr[level], Node.val)
+
+            preOrder(Node.left, level+1)
+            preOrder(Node.right, level+1)
         
-        result = []
-        queue = deque([root])
+        preOrder(root, 0)
+        return arr
         
-        while queue:
-            curr_level_size = len(queue)
-            max_val = float('-inf')
-            
-            for _ in range(curr_level_size):
-                node = queue.popleft()
-                max_val = max(max_val, node.val)
-                
-                for child in [node.left, node.right]:
-                    if child:
-                        queue.append(child)
-            
-            result.append(max_val)
-        
-        return result
