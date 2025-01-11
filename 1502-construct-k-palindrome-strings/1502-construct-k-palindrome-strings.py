1d83bd7e8a8cf2019752579871c31110c59d949a
Raman Pandey <102546029+R-A-M-A-N-P-A-N-D-E-Y@users.noreply.github.com>
2025-01-12 02:19:15 +0530

    Time: 27 ms (93.55%), Space: 18.3 MB (9.32%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
@@ -1,19 +1,10 @@
+from collections import Counter
+
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
         if len(s) < k:
             return False
-        sorted_s = sorted(s)
-        
-        odd_count = 0
-        i = 0
-        
-        while i < len(sorted_s):
-            char = sorted_s[i]
-            count = 0
-            while i < len(sorted_s) and sorted_s[i] == char:
-                count += 1
-                i += 1
-            if count % 2 == 1:
-                odd_count += 1
-        
+
+        char_count = Counter(s)
+        odd_count = sum(freq % 2 for freq in char_count.values())
         return odd_count <= k
