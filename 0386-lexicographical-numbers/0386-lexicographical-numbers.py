309cf0f8307734729897018d7a2075ea068cad77
Raman Pandey <102546029+R-A-M-A-N-P-A-N-D-E-Y@users.noreply.github.com>
2025-06-08 14:20:29 +0530

    Time: 15 ms (58.63%), Space: 23 MB (21.94%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,13 +1,9 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        result = []
-        curr = 1
-        for _ in range(n):
-            result.append(curr)
-            if curr * 10 <= n:
-                curr *= 10  # Move to the next depth (append 0 at the end)
-            else:
-                while curr % 10 == 9 or curr + 1 > n:
-                    curr //= 10  # Backtrack by removing the last digit
-                curr += 1  # Increment to the next number in lexicographical order
-        return result
+        l=[] # take list
+        for i in range (1, n+1) :
+            l.append(str(i)) # store all n numbers as string 
+        l.sort() # list is sorted to get lexicographical 
+        for i in range(n):
+            l[i]=int(l[i]) # convert all strings into integers
+        return l
