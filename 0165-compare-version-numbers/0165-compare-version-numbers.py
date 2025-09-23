48bb8190138c8b70ceb2da294b41f15fd0eddaa3
Raman Pandey <102546029+R-A-M-A-N-P-A-N-D-E-Y@users.noreply.github.com>
2025-09-24 00:06:27 +0530

    Time: 0 ms (100%), Space: 17.9 MB (17.28%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.py
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -1,28 +1,17 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        v1 = version1.split(".")
-        v2 = version2.split(".")
+        a, b = version1.split('.'), version2.split('.')
+        if len(a) < len(b):
+            for i in range(len(b) - len(a)):
+                a.append('0')
+        elif len(b) < len(a):
+            for i in range(len(a) - len(b)):
+                b.append('0')
 
-        while len(v1) != 0 and int(v1[-1]) == 0:
-            v1.pop() 
-
-        while len(v2) != 0 and int(v2[-1]) == 0:
-            v2.pop()
-
-        # print(v1, v2)
-
-        while len(v1) != 0 and len(v2) != 0:
-            if int(v1[0]) == int(v2[0]):
-                v1.remove(v1[0])
-                v2.remove(v2[0])
-            elif int(v1[0]) > int(v2[0]):
+        for i in range(len(a)):
+            if int(a[i]) > int(b[i]):
                 return 1
-            elif int(v1[0]) < int(v2[0]):
+            elif int(a[i]) < int(b[i]):
                 return -1
-        
-        if len(v1) == 0 and len(v2) == 0:
-            return 0
-        elif len(v1) == 0:
-            return -1
-        elif len(v2) == 0:
-            return 1
+
+        return 0
