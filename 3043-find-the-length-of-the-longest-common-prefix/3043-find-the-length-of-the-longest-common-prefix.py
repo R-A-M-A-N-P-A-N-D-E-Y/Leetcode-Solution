403cb59c71b6722834fbe43fb96a5043a9191719
Raman Pandey <102546029+R-A-M-A-N-P-A-N-D-E-Y@users.noreply.github.com>
2026-05-21 22:50:17 +0530

    Time: 265 ms (65.2%), Space: 29.9 MB (83.28%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
@@ -1,24 +1,37 @@
 class Solution:
+    def digits(self, x):
+        cnt = 0
+        while x > 0:
+            cnt += 1
+            x //= 10
+        return cnt
+
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        prefix_map = {}
-        
-        # Step 1: Build the prefix map for arr1
+        prefixes = set()
+
+        # storing all prefixes of arr1
         for num in arr1:
-            str_num = str(num)
-            prefix = ""
-            for ch in str_num:
-                prefix += ch
-                prefix_map[prefix] = prefix_map.get(prefix, 0) + 1
-        
-        max_length = 0
-        
-        # Step 2: Check for common prefixes in arr2
+            x = num
+            while x > 0:
+                prefixes.add(x)
+                x //= 10
+
+        ans = 0
+
+        # check prefixes of arr2 numbers
         for num in arr2:
-            str_num = str(num)
-            prefix = ""
-            for ch in str_num:
-                prefix += ch
-                if prefix in prefix_map:
-                    max_length = max(max_length, len(prefix))
-        
-        return max_length
+            x = num
+            len_ = self.digits(num)
+
+            # checking from larger => smaller
+            while x > 0:
+                if x in prefixes:
+                    ans = max(ans, len_)
+                    # first match is the longest
+                    # so we stop
+                    break
+
+                x //= 10
+                len_ -= 1
+
+        return ans
