eb9e974bdb5c4557e7d17ba1b71af61e463ada94
Raman Pandey <102546029+R-A-M-A-N-P-A-N-D-E-Y@users.noreply.github.com>
2026-07-01 22:15:41 +0530

    Time: 2502 ms (91.62%), Space: 24.9 MB (100%) - LeetHub

--- 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.py
@@ -1,57 +1,40 @@
 class Solution:
-    def __init__(self):
-        self.roww = [0, 0, -1, 1]
-        self.coll = [-1, 1, 0, 0]
+    dirs = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
-    def bfs(self, grid, score, n):
-        q = deque()
+    def maximumSafenessFactor(self, A: List[List[int]]) -> int:
+        if A[0][0] or A[-1][-1]: return 0
+        n, q = len(A), deque()
 
         for i in range(n):
             for j in range(n):
-                if grid[i][j]:
-                    score[i][j] = 0
+                if A[i][j]:
                     q.append((i, j))
 
         while q:
-            x, y = q.popleft()
-            s = score[x][y]
+            i, j = q.popleft()
+            v = A[i][j]
 
-            for i in range(4):
-                new_x = x + self.roww[i]
-                new_y = y + self.coll[i]
+            for dx, dy in self.dirs:
+                x, y = i + dx, j + dy
 
-                if 0 <= new_x < n and 0 <= new_y < n and score[new_x][new_y] > s + 1:
-                    score[new_x][new_y] = s + 1
-                    q.append((new_x, new_y))
+                if min(x, y) >= 0 and max(x, y) < n and not A[x][y]:
+                    A[x][y] = v + 1
+                    q.append((x, y))
 
-    def maximumSafenessFactor(self, grid):
-        n = len(grid)
-        if grid[0][0] or grid[n - 1][n - 1]:
-            return 0
-
-        score = [[float('inf')] * n for _ in range(n)]
-        self.bfs(grid, score, n)
-
-        vis = [[False] * n for _ in range(n)]
-        pq = [(-score[0][0], 0, 0)]
-        heapq.heapify(pq)
+        pq = [(-A[0][0], 0, 0)]
 
         while pq:
-            safe, x, y = heapq.heappop(pq)
-            safe = -safe
+            sf, i, j = heapq.heappop(pq)
+            sf = -sf
 
-            if x == n - 1 and y == n - 1:
-                return safe
+            if i == n - 1 and j == n - 1:
+                return sf - 1
 
-            vis[x][y] = True
+            for dx, dy in self.dirs:
+                x, y = i + dx, j + dy
 
-            for i in range(4):
-                new_x = x + self.roww[i]
-                new_y = y + self.coll[i]
+                if min(x, y) >= 0 and max(x, y) < n and A[x][y] > 0:
+                    heapq.heappush(pq, (-min(sf, A[x][y]), x, y))
+                    A[x][y] *= -1
 
-                if 0 <= new_x < n and 0 <= new_y < n and not vis[new_x][new_y]:
-                    s = min(safe, score[new_x][new_y])
-                    heapq.heappush(pq, (-s, new_x, new_y))
-                    vis[new_x][new_y] = True
-
-        return -1
+        return A[n - 1][n - 1] - 1
