17b56fcb1fae172c366a2f0a95d32b9c586a7a4a
Raman Pandey <102546029+R-A-M-A-N-P-A-N-D-E-Y@users.noreply.github.com>
2026-07-12 14:34:18 +0530

    Time: 41 ms (52.7%), Space: 37.5 MB (60.61%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -1,14 +1,10 @@
 class Solution:
-    def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        value_to_rank = {}  # Dictionary to store value-to-rank mapping
-        sorted_unique_numbers = sorted(list(set(arr)))  # Remove duplicates and sort unique elements
-        
-        # Assign ranks to sorted unique elements
-        for index in range(len(sorted_unique_numbers)): 
-            value_to_rank[sorted_unique_numbers[index]] = index + 1
-          
-        # Replace each element in the original array with its rank
-        for index in range(len(arr)): 
-            arr[index] = value_to_rank[arr[index]]
-        
-        return arr  # Return the updated array
+    def arrayRankTransform(self, arr: list[int]) -> list[int]:
+        ranks = {}
+        rank = 1
+        for x in sorted(list(set(arr))):
+            ranks[x] = rank
+            rank += 1
+        for i in range(len(arr)):
+            arr[i] = ranks[arr[i]]
+        return arr
