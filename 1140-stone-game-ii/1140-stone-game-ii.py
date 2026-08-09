e943f9229a9b9e73b1d002806c366f7b70f1e29a
Raman Pandey <102546029+R-A-M-A-N-P-A-N-D-E-Y@users.noreply.github.com>
2026-08-09 22:39:00 +0530

    Time: 66 ms (95.59%), Space: 26.1 MB (52.86%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.py
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -1,28 +1,13 @@
 class Solution:
-    def helper(self, piles, dp, suffixSum, i, M):
-        if i == len(piles):
-            return 0
-        if i + 2 * M >= len(piles):
-            return suffixSum[i]
-
-        if dp[i][M] != 0:
-            return dp[i][M]
-
-        result = 0
-        for x in range(1, 2 * M + 1):
-            result = max(result, suffixSum[i] - self.helper(piles, dp, suffixSum, i + x, max(M, x)))
-
-        dp[i][M] = result
-        return result
-
-    def stoneGameII(self, piles):
-        if not piles:
-            return 0
-        dp = [[0] * len(piles) for _ in range(len(piles))]
-
-        suffixSum = [0] * len(piles)
-        suffixSum[-1] = piles[-1]
+    def stoneGameII(self, piles: List[int]) -> int:
         for i in range(len(piles) - 2, -1, -1):
-            suffixSum[i] = piles[i] + suffixSum[i + 1]
+            piles[i] += piles[i + 1]
 
-        return self.helper(piles, dp, suffixSum, 0, 1)
+        @cache
+        def dfs(i, M):
+            if i + M * 2 >= len(piles):
+                return piles[i]
+
+            return piles[i] - min(dfs(i + j, max(M, j)) for j in range(1, M * 2 + 1))
+
+        return dfs(0, 1)
